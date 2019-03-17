e4fe5c58d83329f3c571a6a042130ea7c59b7263
Gor Martsen <gor.martsen@gmail.com>
2019-03-17 16:52:10 -0400

    Initial commit for tag-shadow

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const debug = {
+  log: require('debug')('tag-shadow:log'),
+  debug: require('debug')('tag-shadow:debug')
+};
+
+module.exports = {
+  name: 'shadow',
+  type: 'request',
+  handler: function(taggedTargets, config, allTargets, targetRequest){
+    debug.debug('processing shadow %O', config)
+    let voteSize = 1000
+    if (config && config.voteSize) {
+      voteSize = config.voteSize
+    }
+    if (!targetRequest.requestDetails.headers
+      || targetRequest.requestDetails.headers['x-shadow-request']) {
+      debug.debug('non shadow request %O', targetRequest)
+      for (let i in taggedTargets) {
+        // For non shadow request, all shadow tags are voting down.
+        taggedTargets[i].vote -= voteSize
+      }
+      return
+    }
+    for (let i in taggedTargets) {
+      // For shadow request, all shadow tags are voting up.
+      taggedTargets[i].vote += voteSize
+    }
+  }
+}
